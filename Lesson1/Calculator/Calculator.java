5644dd0e3e9bffe5988b7fd2a4055eab58d1ae62
Piligrim <Devastated.life.13@gmail.com>
2018-10-01 22:13:36 +0700

    v1.00

--- Lesson1/Calculator/Calculator.java
+++ Lesson1/Calculator/Calculator.java
@@ -4,26 +4,26 @@ public class Calculator {
  
     public static void main(String[] args) {
        
-    Scanner in = new Scanner(System.in);
-    try{
-    System.out.println("Введите первое число");
-    Double num1 = in.nextDouble();
-    System.out.println("Вы ввели: " + num1);
-    System.out.println("Введите второе число");
-    Double num2 = in.nextDouble();
-    System.out.println("Вы ввели: " + num2);
-    System.out.println("Введите математический символ + , - , / , * или % ");
-    String oper = in.next();
-    System.out.println("Вы ввели: " + oper);
-        switch (oper) {
-            case "-": System.out.println("Выбранная операция - вычитание, " + num1 + " - " + num2 + " = " + (num1 - num2)); break;
-            case "+": System.out.println("Выбранная операция - сложение, " + num1 + " + " + num2 + " = " + (num1 + num2)); break;
-            case "/": System.out.println("Выбранная операция - деление, " + num1 + " / " + num2 + " = " + (num1 / num2)); break;
-            case "*": System.out.println("Выбранная операция - умножение, " + num1 + " * " + num2 + " = " + (num1 * num2)); break;
-            case "%": System.out.println("Выбранная операция - деление по модулю, " + num1 + " % " + num2 + " = " + (num1 % num2)); break;
-            default: System.out.println("Данный символ не включен в функционал нашего калькулятора");
-            }
-        }
-    catch(Exception ex) {System.out.println("Некорректный ввод");}
+    	Scanner in = new Scanner(System.in);
+	    try{
+	    System.out.println("Введите первое число!");
+	    Double num1 = in.nextDouble();
+	    System.out.println("Вы ввели: " + num1);
+	    System.out.println("Введите второе число!");
+	    Double num2 = in.nextDouble();
+	    System.out.println("Вы ввели: " + num2);
+	    System.out.println("Введите математический символ + , - , / , * или % ");
+	    String oper = in.next();
+	    System.out.println("Вы ввели: " + oper);
+	        switch (oper) {
+	            case "-": System.out.println("Выбранная операция - вычитание, " + num1 + " - " + num2 + " = " + (num1 - num2)); break;
+	            case "+": System.out.println("Выбранная операция - сложение, " + num1 + " + " + num2 + " = " + (num1 + num2)); break;
+	            case "/": System.out.println("Выбранная операция - деление, " + num1 + " / " + num2 + " = " + (num1 / num2)); break;
+	            case "*": System.out.println("Выбранная операция - умножение, " + num1 + " * " + num2 + " = " + (num1 * num2)); break;
+	            case "%": System.out.println("Выбранная операция - деление по модулю, " + num1 + " % " + num2 + " = " + (num1 % num2)); break;
+	            default: System.out.println("Данный символ не включен в функционал нашего калькулятора");
+	            }
+	        }
+	    catch(Exception ex) {System.out.println("Некорректный ввод");}
     }           
 }
