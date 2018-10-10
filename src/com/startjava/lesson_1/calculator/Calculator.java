42e93809f29cc6b8f4da8236dbc99ee172d42c5b
Piligrim <Devastated.life.13@gmail.com>
2018-10-10 21:37:01 +0700

    cleaning

--- src/com/startjava/lesson_1/calculator/Calculator.java
+++ src/com/startjava/lesson_1/calculator/Calculator.java
@@ -1,25 +1,21 @@
+package com.startjava.lesson_1.calculator;
+import java.util.Scanner;
+ 
 public class Calculator {
  
-    	
-	    
-	    private double num1;
-	    private String oper;
-	    private double num2;
-
-	    public void setNum1(double num1) {
-	    	this.num1 = num1;
-	    	System.out.println("Вы ввели: " + num1);}
-	    
-	    public void setOper(String oper) {
-	    	this.oper = oper;
-	    	System.out.println("Вы ввели: " + oper);}
-	    
-	    public void  setNum2(double num2) {
-	    	this.num2 = num2;
-	    	System.out.println("Вы ввели: " + num2);}
-	    
-	    
-	    public void calculate() {
+    public static void main(String[] args) {
+       
+    	Scanner in = new Scanner(System.in);
+	    try{
+	    System.out.println("Введите первое число!");
+	    double num1 = in.nextDouble();
+	    System.out.println("Вы ввели: " + num1);
+	    System.out.println("Введите второе число!");
+	    double num2 = in.nextDouble();
+	    System.out.println("Вы ввели: " + num2);
+	    System.out.println("Введите математический символ + , - , / , * , ^ или % ");
+	    String oper = in.next();
+	    System.out.println("Вы ввели: " + oper);
 	        switch (oper) {
 	            case "-": System.out.println("Выбранная операция - вычитание, " + num1 + " - " + num2 + " = " + (num1 - num2)); break;
 	            case "+": System.out.println("Выбранная операция - сложение, " + num1 + " + " + num2 + " = " + (num1 + num2)); break;
@@ -27,8 +23,9 @@ public class Calculator {
 	            case "*": System.out.println("Выбранная операция - умножение, " + num1 + " * " + num2 + " = " + (num1 * num2)); break;
 	            case "^": System.out.println("Выбранная операция - возведение в степень, " + num1 + " ^ " + num2 + " = " + Math.pow(num1,num2)); break;
 	            case "%": System.out.println("Выбранная операция - деление по модулю, " + num1 + " % " + num2 + " = " + (num1 % num2)); break;
-	            default: System.out.println("Данный знак математической операции не включен в функционал нашего калькулятора");
+	            default: System.out.println("Данный символ не включен в функционал нашего калькулятора");
+	            }
 	        }
-	    }
+	    catch(Exception ex) {System.out.println("Некорректный ввод");}
+    }           
 }
-          
